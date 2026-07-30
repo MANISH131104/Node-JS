467cf1ba01628a54bc2b9f66d9969920f77142f6
manish131104 <manishkumar117694@gmail.com>
2026-07-31 00:03:18 +0530

    Updated project

--- Hello_World/file.js
+++ Hello_World/file.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
+const os = require('os');
+
+// console.log(os.cpus().length);      // 12 it means 12 thread sizes can possible
 
 
-// SYNC..
+
+// SYNC.. BLOCKING.....
 // fs.writeFileSync('./test.txt', "Hey There")
 
-// ASYNC..
-//fs.writeFile('./test.txt', "Hey World Async", (err) => {})
+// ASYNC..NON-BLOCKING REQ.....
+// fs.writeFile('./test.txt', "Hey World Async", (err) => {})
 
 
 // const result = fs.readFileSync("./contacts.txt", "utf-8")
@@ -20,10 +24,28 @@ const fs = require('fs');
 //     }
 // })
 
-fs.appendFileSync("./test.txt", `${Date.now()} Hey There\n`);
+// fs.appendFileSync("./test.txt", `${Date.now()} Hey There\n`);
 
-// fs.cpSync('./test.txt', './copy.text')
+// fs.cpSync('./test.txt', './copy.text')   // Used for copy file
 
 // fs.unlinkSync("./copy.text");           // used for deleting file
-console.log(fs.statSync('./test.txt'))
-fs.mkdirSync('my-docss/a/b', {recursive: true}) 
+// console.log(fs.statSync('./test.txt'))
+// fs.mkdirSync('my-docss/a/b', {recursive: true}) 
+
+
+ console.log("1");
+
+// Non-Blocking.....
+fs.readFile("./contacts.txt","utf-8", (err,result) => {
+    console.log(result);
+});
+
+ console.log("2");
+
+
+// Default Thread Pool Size = 4
+// Max ? - 8core cpu - 8
+
+
+
+
